763ae24f37b9b6636dfdb37b213912a1e3fc9019
Artthur0 <vael.253@gmail.com>
2026-04-07 23:36:54 -0400

    cosas

--- resources/views/layout/VistaMusica.blade.php
+++ resources/views/layout/VistaMusica.blade.php
@@ -5,17 +5,30 @@
 <body>
     <section>
         <div class="container" id="Musica">
+            <nav class="navbar" id="Navbar">
+                <h1>Musica</h1>
+                <div class="logo"></div>
+                <div class="search-bar">
+                    <input type="text" placeholder="Buscar productos...">
+                    <button>🔍</button>
+                </div>
+                <div class="cart-icon" onclick="toggleCart()">
+
+                </div>
+            </nav>
+
             <div class="row">
                 <div class="col-md-4">
-                    <h1>Titulo</h1>
+                    <h3>Titulo</h3>
                     <ul>
                         @foreach ($Titulo as $item)
+                        <img id="icono" src="https://cdn-icons-png.flaticon.com/128/876/876817.png" alt="">
                         {{ $item }} <br>
                         @endforeach
                     </ul>
                 </div>
                 <div class="col-md-4">
-                    <h1>Artista</h1>
+                    <h3>Artista</h3>
                     <ul>
                         @foreach ($Artista as $item)
                         {{ $item }} <br>
@@ -23,7 +36,7 @@
                     </ul>
                 </div>
                 <div class="col-md-4">
-                    <h1>Fecha de creación</h1>
+                    <h3>Fecha de creación</h3>
                     <ul>
                         @foreach ($Fecha as $item)
                         {{ $item }} <br>
@@ -39,6 +52,11 @@
 
 
 <style>
+    #icono {
+        width: 25px;
+        height: 25px;
+    }
+
     .container {
         background-color: #f5f5f5;
         padding: 20px;
